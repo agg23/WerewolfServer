819850b297bd7363459397bb8ad7315799ccdd30
Adam Gastineau <adam@appcannonsoftware.com>
2017-08-20 20:00:23 -0500

    Added poor random substitute for compilation on Linux

--- Sources/App/Game/Utility/GeneralExtensions.swift
+++ Sources/App/Game/Utility/GeneralExtensions.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+func cs_arc4random_uniform(_ upperBound: UInt32) -> UInt32 {
+    #if os(Linux)
+        // TODO: Fix
+        return UInt32(random()) % upperBound
+    #else
+        return arc4random_uniform(upperBound)
+    #endif
+}
+
 // From https://stackoverflow.com/a/24029847
 extension MutableCollection where Indices.Iterator.Element == Index {
     /// Shuffles the contents of this collection.
@@ -16,7 +25,7 @@ extension MutableCollection where Indices.Iterator.Element == Index {
         guard c > 1 else { return }
 
         for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let d: IndexDistance = numericCast(cs_arc4random_uniform(numericCast(unshuffledCount)))
             guard d != 0 else { continue }
             let i = index(firstUnshuffled, offsetBy: d)
             swap(&self[firstUnshuffled], &self[i])
